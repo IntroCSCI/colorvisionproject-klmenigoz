c907a1a992e80db229d8b546ffb4ffee1fe391a0
Kent Menigoz <klmenigoz@mail.csuchico.edu>
2020-11-08 03:19:45 +0000

    added/moved old functionality into dedicated functions

--- main.cpp
+++ main.cpp
@@ -4,34 +4,60 @@
 
 using namespace std;
 
-char chooseMenu();
-void runSearchFunct();
+int chooseMenu();
+void runSearchFunct(string);
+string editSearchTerm(string);
 
+
+string searchTerm = "color:#";
+     
 int main(){ ////////////////////////////////// MAIN //////////////////////////////////////
-  
-  int red;      //r g and b are not 
-  int blue;     //    yet being used
-  int green;
-  char choice;
-  fstream reader;
-  string hot = "color:#";    //<---important for changing search term, add FUNCTION HERE
 
-
-
-  char input;
+  int input;
+  do{
   input = chooseMenu();
 
-do{
+if(input == 1){
+   runSearchFunct(searchTerm);
+   }
+  if(input == 2){
+    string currentTerm = searchTerm;
+    searchTerm = editSearchTerm(currentTerm);
+    }
+  if(input == 3){
+    //int editReadoutLength()
+    }
+  }while(input != 4);
+  return 0;
+}
 
-if(input == 'o' || input == 'O'){
-   runSearchFunct();
-  }
 
-}while( input != 'q' && input != 'Q' );
 
-do
-{
+
+
+
+//////////////////         FUNCTIONS             //////////////////  
+//////////////////            //                 //////////////////
+
+int chooseMenu(){                  /////-----chooseMenu function
+  int input;
+    cout << "\n Menu\n------\n";
+    cout << "(1) Open new file\n";
+    cout << "(2) Edit search term\n";
+    cout << "(3) Adjust colorInfo printout length\n";
+    cout << "(4) Quit program\n";
+
+    cin >> input;
+    cin.ignore();
+    return input;
+}
+
+void runSearchFunct(string term){            /////------runSearchFunct function
+ 
   string fileName;
+  char choice;
+  fstream reader;
+  string hot = term;    //<---important for changing search term, add FUNCTION HERE
   
   cout<<endl<<"Enter a file name to check its colors"<<endl;
   cin>>fileName;
@@ -43,12 +69,14 @@ do
       for (int lineNum = 0; lineNum < 250; lineNum++){    //read filename by line (250                                                         //                     lines)
         getline(reader, text);                            //str "text" is now the current                                                    //line
 
-        int x = text.find(hot);       //search for "hot" term, in this case "color:"
+        int x = text.find(hot);       //search for "hot" term, in this case "color:#"
         if (x != -1){                 //  .find outputs certain logic  ¯\_(ツ)_/¯
             string colorInfo = text.substr (x, 13);    //create substring colorInfo
               if (colorInfo != " " ){                  //
-                 
-                  cout<< colorInfo <<endl;
+
+                  cout<< colorInfo <<endl;             //print colorInfo
+
+
           }
           }
       }
@@ -57,32 +85,23 @@ do
    { cout<<"Could not open file\n";
    }
    reader.close();
-  cout<<"Analyze another file? (y/n)"<<endl;
-  
-  cin>>choice;
-  
-}while(choice != 'n');
-
-  return 0;
+  return;
 }
 
 
-char chooseMenu(){                  //chooseMenu function
-  char input;
-    cout << "Menu\n----\n";
-    cout << "(O)pen new file\n";
-    cout << "(E)dit search term\n";
-
-    cout << "(Q)uit program\n";
-
-    cin >> input;
-    cin.ignore();
-    return input;
-}
-
-
-void runSearchFunct(){              //search function
-
-
+string editSearchTerm(string current){
+cout<< "\nThe curent search term is:\n"<<current<<endl;
+cout<< "Would you like to change the search term? (y/n)\n";
+char choice;
+cin>> choice;
+if(choice == 'y'){
+ cout<<"Enter the new search term:\n";
+ string newTerm;
+ cin>> newTerm;
+ cout<< "New search term:\n"<< newTerm << endl;
+ return newTerm;
+}else{
+  return searchTerm;
+ }
 
 }
